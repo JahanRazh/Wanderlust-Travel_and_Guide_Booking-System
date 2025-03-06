40f8f86564803662336229e92a027ce0ea974a80
JAHAN RAMESH <jahanrazh@gmail.com>
2025-03-06 23:55:11 +0530

    Update title and MainNavbar branding to "Wanderlust"

--- Frontend/src/components/MainNavbar.jsx
+++ Frontend/src/components/MainNavbar.jsx
@@ -6,7 +6,8 @@ export default function MainNavbar() {
             <nav className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
                 <div className="flex items-center">
                     <img src={LOGO} alt="Logo" className="h-15" />
-                    <h1 className="text-white font-bold text-xl">Travel</h1>
+                    <h1 className="text-2xl font-bold p-3"><span className="text-blue-500">Wander</span>lust</h1>
+                    <h1 className="text-2xl font-bold p-3">Wander<span className="text-blue-500">lust</span></h1>
                 </div>
                 <ul className="hidden md:flex">
                     <li className="mx-2"><a href="#" className="text-black hover:text-gray-300">Home</a></li>
