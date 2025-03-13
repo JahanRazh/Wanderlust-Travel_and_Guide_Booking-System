915de40d6a5392b3cc4a8adeec798498f5273b3a
JAHAN RAMESH <jahanrazh@gmail.com>
2025-03-13 10:53:46 +0530

    Add routes for forgot and reset password functionality

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,3 +1,4 @@
+// authRoutes.js
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
@@ -7,5 +8,11 @@ router.post("/create-account", authController.register);
 
 // Route to handle user login
 router.post("/login", authController.login);
+
+// Route to handle forgot password
+router.post("/forgot-password", authController.forgotPassword);
+
+// Route to handle reset password
+router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;
