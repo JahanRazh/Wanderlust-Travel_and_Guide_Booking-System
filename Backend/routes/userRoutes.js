04e46450a67802fdd4d554db82e722e044683381
JAHAN RAMESH <jahanrazh@gmail.com>
2025-03-18 17:42:40 +0530

    Add route for updating user profile

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const { authenticateToken } = require("../middleware/auth");
+const upload = require("../middleware/upload");
 
 // Route to get user details (requires authentication)
 router.get("/get-user", authenticateToken, userController.getUser);
 
-module.exports = router;
 
+// Update user profile
+router.put("/user/update-profile", authenticateToken, userController.updateProfile);
+
+module.exports = router;
