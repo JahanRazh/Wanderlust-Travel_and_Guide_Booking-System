387d86f17fdad78044dc58114e26051a6b6777e1
JAHAN RAMESH <jahanrazh@gmail.com>
2025-03-23 21:10:47 +0530

    Implement admin middleware and remove unused package models and controllers

--- Backend/models/Admin/hotelModel.js
+++ Backend/models/Admin/hotelModel.js
@@ -1,15 +1,1 @@
-const mongoose = require("mongoose");
 
-const packageSchema = new mongoose.Schema(
-  {
-    packageName: { type: String, required: true },
-    pricePerPerson: { type: Number, required: true },
-    hotel: { type: String, required: true },
-    guide: { type: String, required: true },
-    description: { type: String, required: true },
-    climate: { type: String, required: true },
-  }
-);
-
-const Package = mongoose.model("Package", packageSchema);
-module.exports = Package;
